2eb8bcdf3ac22759089645f50ccae1ab8175cdae
Chakradhar Dwivedi <chakradhar.dwivedi2000@gmail.com>
2024-01-31 11:35:06 +0530

    First try

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::i32;
+
+fn main() {
+   println!("Hello, world!");
+   let mut x : i32 = 32;
+   println!("x : {x}");
+   x=20;
+   println!("x : {x}");
+   x=423;
+   println!("x : {x}");
+   //    print!("Product :{} ", product(2,10,5));
+//   let part1 : &str = "Hello Chakru";
+//   let part2 :&str = "📢"; 
+//   let mut str = String::new();
+//   str.push_str(part1);
+//   str.push_str(" ");
+//   str.push_str(part2);
+//   println!("Final Sentence : {}" , str);
+//   println!("{:?}", &str[0..3]);
+//   println!("{:?}", &str[10..13]);
+//
+   let mut y : i32 = 11;
+   println!("y : {y}");
+//   swap(&x,&y);
+}
+
+//fn swap(x:&i32,y:&i32)-> (){
+//  let mut temp : i32 = 0;
+//   temp = &x;
+//   x=y;
+//   y = &temp;
+//}
+
+//fn product(a:i32,b:i32,c:i32) -> i32{
+//   return a*b + b*c + c*a;  
+//}
+//
